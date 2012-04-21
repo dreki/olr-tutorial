e31d77a15c0efaf08fc3721dfe0d6419fd7837ee
Sean Gilbertson <sean.gilbertson@gmail.com>
2012-04-21 16:26:37 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Olr::Application.config.secret_token = '6948026dfd01435823645f13e98208a774100be74df252bc3680c22ea49da3b54bc8b3e81ab89ff38d333c2130a2f18b313751c2e31e38eb366f875c814e1675'
